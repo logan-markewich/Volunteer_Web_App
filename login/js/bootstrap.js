d1f41c9508dba1102d473137b771126c7bd6c0c5
jin <jin.2088@usask.ca>
2017-10-02 20:04:14 -0600

    update bootstrap.js

--- login/js/bootstrap.js
+++ login/js/bootstrap.js
@@ -25,6 +25,8 @@ if (!jQuery) { throw new Error("Bootstrap requires jQuery") }
  * ======================================================================== */
 
 
+
+
 +function ($) { "use strict";
 
   // CSS TRANSITION SUPPORT (Shoutout: http://www.modernizr.com/)

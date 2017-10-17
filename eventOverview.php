c6639c9c1d7062f7c620242b9a14d48e9ca8f4b9
bradnim <32319639+bradnim@users.noreply.github.com>
2017-10-17 10:30:27 -0600

    Add files via upload

---
+++ eventOverview.php
@@ -0,0 +1,120 @@
+<?php session_start(); 
+require('database.php');
+$sql = ("SELECT * FROM cmpt370_rdynam.events WHERE idEvent='" . $_GET['id'] . "'");
+$result = mysqli_query($conn, $sql);
+$_SESSION['id'] = $_GET['id'];
+while($row = $result -> fetch_assoc()){
+$_SESSION["eventName"]=$row["eventName"];
+$_SESSION["location"]=$row["location"];
+$_SESSION["startDate"]=$row["startDate"];
+$_SESSION["endDate"]=$row["endDate"];
+$_SESSION["numShifts"]=$row["numShifts"];
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Hollandia Volunt-EZ</title>
+	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="/css/main.css">
+	<link rel="stylesheet" href="/css/menu.css">
+	<link rel="stylesheet" href="/css/hdr_ftr.css">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<?php	if($_SESSION["username"] == NULL){
+			header("Location: index.php");
+		}
+		?>
+<body>
+	<!-- Page Header Information -->
+	<div class="container-fluid" id="contain" >
+		<div id="hdr">
+			<div id="social">
+				<a href="https://www.facebook.com/hollandiasoccer" target="_blank">			
+					<img src="img/fb.png" />
+				</a>
+				<a href="https://www.instagram.com/hollandiasoccer/" target="_blank">		
+					<img src="img/ig.png" >
+				</a>
+				<a href="https://www.youtube.com/channel/UCeyZXkqKTUyNs0S_tCL9QFw" target="_blank">
+					<img src="img/yt.png" >
+				</a>
+				<a href="https://twitter.com/HollandiaSoccer" target="_blank">
+					<img src="img/tw.png" >
+				</a>
+			</div>
+		</div>
+		<div id="mnu">
+			<?php include('menu2.php'); ?>
+		</div>
+		<div id="Main">
+		<div class="row" id="dashOptions">
+			<div class="col-sm-12">
+				<h1 id="welcome"> <?php echo($_SESSION["eventName"]); ?></h1>
+			</div>
+		</div>
+		
+		<div class="row" id="dashData" align="left" style="padding-left:10px">
+			<div class="col-sm-7">
+			<h2> Event Details </h2>
+			<div id="edit">
+			<h3i>Location: <?php echo $_SESSION['location']; ?></h3i>
+			<button> Edit </button>
+			</div>
+			<div>
+			<h3i>Dates: <?php echo date('F j, Y',strtotime($_SESSION['startDate']))." - ".date('F j, Y',strtotime($_SESSION['endDate'])); ?></h3i>
+			<button> Edit </button>
+			</div>
+			<div>
+			<div>
+			<h3i>Number of Shifts: <?php 
+			if ($_SESSION['numShifts'] == NULL){
+				echo "0";
+			}
+			else{
+				echo $_SESSION['numShifts']; 
+			}
+			?></h3i>
+			</div>
+
+		</div>
+		</div>
+			<div class="col-sm-5">
+			<h2>Shift Information</h2>
+			<div>
+			<button> Manage Shift Types/Decriptions </button>
+			</div>
+			<div>
+			<button> Add Shifts </button>
+			</div>
+			<div>
+			<button> Manage Existing Shifts </button>
+			</div>
+		</div>
+			
+		<!-- Footer Information -->
+		<div class="col-sm-12" id="ftr">
+        	<center>
+        		<div id="mob-social">
+					<a href="https://www.facebook.com/hollandiasoccer" target="_blank"><img src="img/fb.png" /></a><a href="https://twitter.com/HollandiaSoccer" target="_blank"><img src="img/tw.png" ></a><a href="https://www.instagram.com/hollandiasoccer/" target="_blank"><img src="img/ig.png" ></a><a href="https://www.youtube.com/channel/UCeyZXkqKTUyNs0S_tCL9QFw" target="_blank"><img src="img/yt.png" ></a>
+				</div>
+				<p>
+					<strong>Purpose</strong>: <em>Enhancing our community by inspiring people to enrich their lives through soccer</em><br />
+					<strong>Mission</strong>: <em>To inspire players, parents, and coaches to be passionate about soccer and to reach their full potential through player-centric programs</em><br />
+            		<strong>Vision</strong>: <em>Hollandia will be the club of choice, building community and a membership which commits to behavior consistent with our values</em>
+				</p>
+        		<p><span>Copyright Hollandia Soccer Club; <? echo date("Y"); ?>  	All Rights Reserved.</span></p>
+        	</center>
+		</div>
+	</div>
+
+</body>
+</html>
---
+++ menu2.php
@@ -0,0 +1,16 @@
+<div class="menu" id="menu">
+	<ul>
+		<li><a href="dashboard.php">Home</a></li>
+		<li><a href="eventOverview.php?id=<?php echo $_SESSION['id'];?>"><?php echo $_SESSION['eventName'];?></a></li>
+        <li><a href="#">Manage Shifts</a><b aria-haspopup="true" aria-controls="p1"></b>
+        	<ul id="p1">
+				<li><a href="#"> Shift Types/Descriptions</a></li>
+				<li><a href="#"> Add Shifts</a></li>
+				<li><a href="#"> Manage Existing Shifts</a></li>
+				</ul>
+        </li>
+		<li><a href="#">Manage Volunteers</a></li>
+		<li><a href="logout.php">Logout</a></li>
+
+	</ul>
+</div>

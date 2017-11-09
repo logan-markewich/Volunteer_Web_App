649ac55cfefb2d6c0233ef329a3e8662d4a427ea
Jin <31224222+jit2088@users.noreply.github.com>
2017-11-09 09:22:28 -0600

    Add files via upload
    delete event from event table
    drop shifts table at event

--- deleteEvent.php
+++ deleteEvent.php
@@ -4,6 +4,24 @@ session_start();
 
 $sql = "DELETE FROM events WHERE eventName = '" . $_SESSION["eventName"] . "' ";
 
+
+if (mysqli_query($conn, $sql)) {
+    header("Location: dashboard.php");
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?> 
+
+
+<?php
+require('database.php');
+session_start();
+
+
+$sql = "DROP TABLE  " . $_SESSION["eventName"] . " ";
+
 if (mysqli_query($conn, $sql)) {
     header("Location: dashboard.php");
 } else {

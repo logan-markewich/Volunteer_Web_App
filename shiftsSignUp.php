09ba3f3e1e059bbd75b9b55e76950978bb6946cc
logan-markewich <logan.markewich@live.com>
2017-11-28 15:27:11 -0600

    matches shifts.php now

--- shiftsSignUp.php
+++ shiftsSignUp.php
@@ -1,7 +1,6 @@
 <?php 
 session_start(); 
 require('./scripts/config/database.php');
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,82 +24,67 @@ require('./scripts/config/database.php');
 	
 	<div class="container-fluid" id="contain">
 		<?php include('hdr.php'); ?>
-		<?php 
-		$sql7 = ("SELECT * FROM cmpt370_rdynam.events WHERE idEvent='" . $_GET['id'] . "'");
-		$result7 = mysqli_query($conn, $sql7);
-		if ($result7) {
-							while($row7 = $result7 -> fetch_assoc()){
-								$_SESSION['eventName'] = $row7['eventName'];
-								
-								
-							}
-						}
 		
-		$table_name = str_replace(' ', '', "cmpt370_rdynam.".$_SESSION['eventName']);
-		$dates = array();
-		$sql = ("SELECT count( DISTINCT(date_Time) ) FROM ". $table_name ."");
-		$result = mysqli_query($conn, $sql);
-		$sql2 = ("SELECT date_Time FROM ". $table_name ."");
-		$result2 = mysqli_query($conn, $sql2);
-		$count = 0;
-		if ($result2) {
-							while($row = $result2 -> fetch_assoc()){
-								array_push($dates, $row['date_Time']);
-								$count += 1;
-							}
-						}
-		asort($dates);
-
-		?>
 		<!-- Php should make a row for every two events -->
 		<div id="Main">
 		
-		<h1> <?php echo($_SESSION["eventName"]); ?> </h1> 
-		<?php 
-		for ($i = 0; $i < $count; $i+=2){
+		<h1> <?php echo($_SESSION["eventName"]); ?> </h1>
+		<?php
+			$table_name = str_replace(' ', '', $_SESSION["eventName"]);
+			$sql = ("select * from ". $table_name ." order by date_time asc");
+			$result = mysqli_query($conn, $sql);
+			$prevRowDate = NULL;
+			$dates = array();
+			$count = 0;
+			$dateCount = 0;
+			$dates[$count] = array();
+			if ($result) {
+				while($row = $result -> fetch_assoc()){
+					//Actually code make a shift area right here
+					if($prevRowDate != NULL) {
+						if($prevRowDate == $row['date_Time']){
+							array_push($dates[$count], $row);
+						}
+						else {
+							$count++;
+							$dates[$count] = array();
+							array_push($dates[$count], $row);
+						}
+					}
+					
+					$prevRowDate = $row["date_Time"];
+				}
+			}
+					
+			foreach($dates as $value) {
 		?>
-		
 		<div class="row" id="shiftRow">
-  			<div class="col-sm-6" id="shifts-left">
-				<h2> <?php echo(date('F j',strtotime($dates[0+$i]))); ?> </h2>
-				<div id="shiftarea">
-					<?php 
-					$sql3 = ("SELECT * FROM ". $table_name ." WHERE date_Time='" . $dates[0+$i] . "'");
-					$result3 = mysqli_query($conn, $sql3);
-					if ($result3) {
-							while($row2 = $result3 -> fetch_assoc()){ ?>
-								<button id="shiftBtn" type="button" onclick="" class="btn btn-primary btn-block">
-								<h3><?php echo date('g:i A',strtotime($row2["start_Time"])) ?> | <?php echo $row2['shift_position'] ?></h3>
-								<h3><?php echo $row2['number_of_volunteers_left'] ?> shifts left</h3>
-								</button> <?php 
-							}
-						} ?>
-				</div>
-			</div>
+  			<div class="col-sm-12" id="shifts-left">
 			<?php 
-				if($count > ($i + 1)) { 
+				$first = 1;
+				foreach($value as $rowValue) {
+					if($first == 1) {
 			?>
-			<div class="col-sm-6" id="shifts-right">
-				<h2> <?php echo(date('F j',strtotime($dates[1+$i]))); ?> </h2>
+				<h2> <?php echo(date('F j',strtotime($rowValue['date_Time']))); ?> </h2>
 				<div id="shiftarea">
-					<?php 
-					$sql4 = ("SELECT * FROM ". $table_name ." WHERE date_Time='" . $dates[1+$i] . "'");
-					$result4 = mysqli_query($conn, $sql4);
-					if ($result4) {
-							while($row3 = $result4 -> fetch_assoc()){ ?>
-								<button id="shiftBtn" type="button" onclick="" class="btn btn-primary btn-block">
-								<h3><?php echo date('g:i A',strtotime($row3["start_Time"])) ?> | <?php echo $row3["shift_position"] ?></h3>
-								<h3><?php echo $row3["number_of_volunteers_left"] ?> shifts left</h3>
-								</button> <?php 
-							}
-						} ?>
-				</div>
-			</div>
+			<?php
+					$first = 0;
+					}
+			?>
+							<button id="shiftBtn" type="button" onclick="" class="btn btn-primary btn-block">
+							<h3><?php echo date('g:i A',strtotime($rowValue["start_Time"])) ?> | <?php echo $rowValue['shift_position'] ?></h3>
+							<h3><?php echo $rowValue['number_of_volunteers_left'] ?> shifts left</h3>
+							</button> 
 			<?php 
 				}
 			?>
+				</div>
+			</div>
 		</div>
-		<?php } ?>
+		<?php
+				$count++;
+			}
+		?>
 		</div>
 		
 		<?php include('ftr.php'); ?>

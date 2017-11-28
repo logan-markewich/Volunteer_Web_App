6add144a9a671d7b02adfe6635e649a80643306f
Jin <31224222+jit2088@users.noreply.github.com>
2017-11-28 02:03:49 -0600

    Add files via upload
    drop down for "shift type" works
    need to polish the style, it looks ugly now

--- createShifts.php
+++ createShifts.php
@@ -1,19 +1,24 @@
-<?php 
-	session_start(); 
-	require('./scripts/config/database.php');
-	$_GET['id'] = $_SESSION['id'];
+<!-- 
+<?php session_start(); 
+require('./scripts/config/database.php');
+$sql = ("SELECT * FROM cmpt370_rdynam.shift_types");
+$result = mysqli_query($conn, $sql);
+$types = array();
 
-	$sql = ("SELECT * FROM cmpt370_rdynam.events WHERE idEvent='" . $_GET['id'] . "'");
-	$result = mysqli_query($conn, $sql);
-	$_SESSION['id'] = $_GET['id'];
-	while($row = $result -> fetch_assoc()){
-		$_SESSION["eventName"]=$row["eventName"];
-		$_SESSION["location"]=$row["location"];
-		$_SESSION["startDate"]=$row["startDate"];
-		$_SESSION["endDate"]=$row["endDate"];
-		$_SESSION["numShifts"]=$row["numShifts"];
-	}
+
+// $sdescrip = "";
+
+// while($row = $result -> fetch_assoc()){
+// 
+// 	$types[$i] = $row['type'];
+// 	
+// 	echo $types[$i];
+// 	}
+// 	
+// 
+// exit;
 ?>
+ -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -38,46 +43,65 @@
 	<div class="container-fluid" id="contain">
 		<?php include('hdr.php'); ?>
 		<div id="mnu">
-			<?php include('menu2.php'); ?>
+			<?php include('scripts/menus/menu2.php'); ?>
 		</div>
+		
 		<!-- Main Content -->
 		<div class="row" id="Main">
 			<div class="col-sm-12" id="volInfo">
+			
 			<h1> <?php echo($_SESSION["eventName"]); ?> <?php echo '-- Shift Information' ?> </h1> 
 				<form class="form-horizontal" name="createShift" action="/scripts/create/newShift.php" method="post">
 					<!-- Event Creation Form -->
+					
+<!-- &&&&&&&					 -->
+					<div class="dropdown_container" >
+					
+    					<label for = "select"> Select Shift Types (select one): </label>
+    					
+    					<select class="form-control" name ="shiftType" >
+    					
+    					<?php 
+						$sql = ("SELECT * FROM cmpt370_rdynam.shift_types ");
+						$result = mysqli_query($conn, $sql);
+						if ($result) {
+							while($row = $result -> fetch_assoc()){
+								echo "<option type = '".$row['type']."'> ".$row['type']."  </option>";
+							}
+						}
+						?>
+    					
+    			
+ 						 </select>
+    					
+    	
+					
+    					
+    					
+    					</div>
+				  </div> 
+    					
+<!--   &&&&&					 -->
+
+					
+					
   					<div class="form-group">
+  					
     					<label class="control-label col-sm-2" for="name">Shift Location:</label>
     					<div class="col-sm-10">
       						<input type="text" name="location" class="form-control" id="shift_sign_up" placeholder="Shift Location">
     					</div>
+    				
   					</div>
-  					<div class="form-group">
-    					<label class="control-label col-sm-2" for="name">Shift Position:</label>
-    					<div class="col-sm-10">
-      						<div class="dropdown">
-							<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown"><span id="selected">Select Type
-						
-					</span><span class="caret"></span></button>
-  					<ul class="dropdown-menu">
-    					<?php 
-						$sql3 = ("SELECT shiftType FROM cmpt370_rdynam.shift_descriptions WHERE idShiftDescrip='" . $_GET['id'] . "'");
-						$result3 = mysqli_query($conn, $sql3);
-						while($row = $result -> fetch_assoc()){
-						
-							?><li><a href="shiftTypes.php?id=<?php echo $row['idShiftDescrip'];?>"><?php echo $row['shiftType']; ?></a></li><?php
-											
-						} ?>
-  					</ul>
-				</div> 
-    					</div>
-  					</div>
+  					
+  					
 					<div class="form-group">
     					<label class="control-label col-sm-2" for="name">Shift Date:</label>
     					<div class="col-sm-10">
       						<input type="date" name="date" class="form-control" id="shift_sign_up" placeholder="17/11/30 for November 30, 2017">
     					</div>
   					</div>
+  					
 					<div class="form-group">
     					<label class="control-label col-sm-2" for="date">Start Time:</label>
     					<div class="col-sm-10">
@@ -98,6 +122,7 @@
       						<input type="integer" name="number_of_vol" class="form-control" id="shift_sign_up" placeholder="Number Of Volunteers">
     					</div>
   					</div>
+  					
   					<div class="form-group"> 
     					<div class="col-sm-offset-2 col-sm-10">
       						<button type="submit" class="btn btn-default" action="/scripts/create/newShift.php" method="post"    >Submit</button>

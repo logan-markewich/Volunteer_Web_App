71435384573f8febf0a06486c15de03524e2e463
logan-markewich <logan.markewich@live.com>
2017-11-28 19:09:35 -0600

    forgot to add event Name to volunteer

--- scripts/create/newVolunteer.php
+++ scripts/create/newVolunteer.php
@@ -4,7 +4,7 @@ session_start();
 
 $eventNameNoSpaces = str_replace(' ', '', $_SESSION["eventName"]);
 
-$sql1 = "insert into volunteers (firstName, lastName, email, phoneNumber, idShift) values ('". $_POST["firstName"] ."', '". $_POST["lastName"] ."', '". $_POST["email"] ."', '". $_POST["phoneNumber"] ."', '". $_POST["shiftId"] ."')";
+$sql1 = "insert into volunteers (firstName, lastName, email, phoneNumber, idShift, eventName) values ('". $_POST["firstName"] ."', '". $_POST["lastName"] ."', '". $_POST["email"] ."', '". $_POST["phoneNumber"] ."', '". $_POST["shiftId"] ."', '". $_SESSION["eventName"] ."')";
 
 if (mysqli_query($conn, $sql1)) {
 	$sql2 = "update ". $eventNameNoSpaces ." set number_of_volunteers_in = number_of_volunteers_in + 1, number_of_volunteers_left = number_of_volunteers_left - 1 where idShift = ". $_POST["shiftId"] ."";

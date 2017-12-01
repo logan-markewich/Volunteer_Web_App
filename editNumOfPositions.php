fb7c453074306e2f1f8649fc2e77f653c0ed07d6
logan-markewich <logan.markewich@live.com>
2017-12-01 16:46:57 -0600

    formatting code

--- editNumOfPositions.php
+++ editNumOfPositions.php
@@ -19,29 +19,27 @@
 </head>
 	
 <body>
-	<!-- Page header: photo and social media -->
 	<div class="container-fluid" id="contain">
+		<!-- Page header: photo and social media -->
 		<?php include('hdr.php'); ?>
+		
 		<!-- Main Content -->
 		<div class="row" id="Main">
 			<div class="col-sm-12" id="volInfo">
 				<form class="form-horizontal" name="editShift" action="/scripts/edit/changeNumOfPositions.php" method="post">
-					<!-- Event Creation Form -->
-  					
+					<!-- Edit Shift Num Positions Form -->			
 					<div class="form-group">
-    					<label class="control-label col-sm-2" for="name">Change number of positions:</label>
-    					<div class="col-sm-10">
-      						<input type="text" name="numOfPostions" class="form-control" id="shift_sign_up" placeholder=" Number of Positions">
-    					</div>
+    						<label class="control-label col-sm-2" for="name">Change number of positions:</label>
+    						<div class="col-sm-10">
+      							<input type="text" name="numOfPostions" class="form-control" id="shift_sign_up" placeholder=" Number of Positions">
+    						</div>
   					</div>
-					
   					<div class="form-group"> 
-    					<div class="col-sm-offset-2 col-sm-10">
-      						<button type="submit" class="btn btn-default">Submit</button>
-    					</div>
+    						<div class="col-sm-offset-2 col-sm-10">
+      							<button type="submit" class="btn btn-default">Submit</button>
+    						</div>
   					</div>
 				</form>
-
 			</div>
 		</div>
 		<!-- Footer Information -->

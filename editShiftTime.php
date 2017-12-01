5d27c1c899037cad984af92716e266ef436ee5f1
logan-markewich <logan.markewich@live.com>
2017-12-01 16:49:24 -0600

    formatting code

--- editShiftTime.php
+++ editShiftTime.php
@@ -16,38 +16,35 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-</head>
-	
+</head>	
 <body>
-	<!-- Page header: photo and social media -->
 	<div class="container-fluid" id="contain">
+		<!-- Page header: photo and social media -->
 		<?php include('hdr.php'); ?>
+		
 		<!-- Main Content -->
 		<div class="row" id="Main">
 			<div class="col-sm-12" id="volInfo">
 				<form class="form-horizontal" name="editShiftTime" action="/scripts/edit/changeShiftTime.php" method="post">
-					<!-- Event Creation Form -->
-  					
+					<!-- Edit shift time Form -->
 					<div class="form-group">
-    					<label class="control-label col-sm-2" for="date">Change Start Time:</label>
-    					<div class="col-sm-10">
-      						<input type="time" name="startTime" class="form-control" id="shift_sign_up" placeholder="  00:00  ">
-    					</div>
+    						<label class="control-label col-sm-2" for="date">Change Start Time:</label>
+    						<div class="col-sm-10">
+      							<input type="time" name="startTime" class="form-control" id="shift_sign_up" placeholder="  00:00  ">
+    						</div>
   					</div>        
-  					
 					<div class="form-group">
-    					<label class="control-label col-sm-2" for="date">Change End Time:</label>
-    					<div class="col-sm-10">
-      						<input type="time" name="endTime" class="form-control" id="shift_sign_up" placeholder="  00:00  ">
-    					</div>
+    						<label class="control-label col-sm-2" for="date">Change End Time:</label>
+    						<div class="col-sm-10">
+      							<input type="time" name="endTime" class="form-control" id="shift_sign_up" placeholder="  00:00  ">
+    						</div>
   					</div>
   					<div class="form-group"> 
-    					<div class="col-sm-offset-2 col-sm-10">
-      						<button type="submit" class="btn btn-default">Submit</button>
-    					</div>
+    						<div class="col-sm-offset-2 col-sm-10">
+      							<button type="submit" class="btn btn-default">Submit</button>
+    						</div>
   					</div>
 				</form>
-
 			</div>
 		</div>
 		<!-- Footer Information -->

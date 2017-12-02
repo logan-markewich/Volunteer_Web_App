56f3e5ab2625666da260c3390076fdbe69ee317e
logan-markewich <logan.markewich@live.com>
2017-12-02 12:33:32 -0600

    formatting code

--- shiftEdit.php
+++ shiftEdit.php
@@ -1,22 +1,23 @@
-<?php session_start(); 
-require('./scripts/config/database.php');
-$table_name = str_replace(' ', '', "cmpt370_rdynam.".$_SESSION['eventName']);
-$sql = ("SELECT * FROM ".$table_name." WHERE idShift='" . $_GET['id'] . "'");
-$result = mysqli_query($conn, $sql);
-while($row = $result -> fetch_assoc()){
+<?php 
+	session_start(); 
+	require('./scripts/config/database.php');
 
-	$_SESSION['shift_id'] = $row['idShift'];
+	$table_name = str_replace(' ', '', "cmpt370_rdynam.".$_SESSION['eventName']);
+	$sql = ("SELECT * FROM ".$table_name." WHERE idShift='" . $_GET['id'] . "'");
+	$result = mysqli_query($conn, $sql);
 	
-	$name = $row['shift_position'];
-	$loc = $row['shift_location'];
-	$start = $row['start_Time'];
-	$end = $row['end_Time'];
-	$date = $row['date_Time'];
-	$numVol = $row['number_of_volunteers'];
-}
+	//Get variables from fetch
+	while($row = $result -> fetch_assoc()){
+		$_SESSION['shift_id'] = $row['idShift'];
+	
+		$name = $row['shift_position'];
+		$loc = $row['shift_location'];
+		$start = $row['start_Time'];
+		$end = $row['end_Time'];
+		$date = $row['date_Time'];
+		$numVol = $row['number_of_volunteers'];
+	}
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,7 +45,6 @@ while($row = $result -> fetch_assoc()){
 		</div>
 		
 		<div class="row" id="Main">
-
 			<div class="col-sm-6" id="shiftEditInfo">
 				<form class="form-horizontal" name="positionForm" action="/scripts/edit/changeShiftName.php" method="post">
 					<div class="form-group" name="positionForm">
@@ -93,61 +93,25 @@ while($row = $result -> fetch_assoc()){
 			
 			<div class="col-sm-6" id="volInfo">
 				<h1> Shift Description </h1>
-				<?php $sql3 = ("SELECT shiftDescription FROM cmpt370_rdynam.shift_descriptions WHERE shiftType='" . $name . "'");
-										$result3 = mysqli_query($conn, $sql3);
+				<?php
+					//Get proper shift description from SQL
+					$sql3 = ("SELECT shiftDescription FROM cmpt370_rdynam.shift_descriptions WHERE shiftType='" . $name . "'");
+					$result3 = mysqli_query($conn, $sql3);
 
-										while($row3 = $result3 -> fetch_assoc()){
-											echo $row3['shiftDescription'];
-										} 
-										 ?>
+					while($row3 = $result3 -> fetch_assoc()){
+						echo $row3['shiftDescription'];
+					} 
+				?>
 			</div>
 		</div>
-		
-		<!-- The Modal -->
-		<div id="editNameModal" class="modal">
-
-			<!-- Modal content -->
-			<div class="modal-content">
-				<span class="close">&times;</span>
-				<input type="text" name="changeName" class="form-control" id="nameChange" placeholder="Shift Name">
-				<button id="editVolBtn" type="submit" class="btn btn-default" onclick="/scripts/edit/changeShiftName.php"> Submit </button>
-			</div>
-		
-<script>// Get the modal
-var modal = document.getElementById('editNameModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("editNameBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-} </script>
-
-<script>	
-$('.dropdown li').click(function(){
-    $('#selected').text($(this).text());
-	$('#position').val($(this).text());
-    $('#positionForm').submit();
-  });			
-</script>
-		</div>
-		
+		<!-- script to update dropdown selection -->
+		<script>	
+			$('.dropdown li').click(function(){
+    				$('#selected').text($(this).text());
+				$('#position').val($(this).text());
+    				$('#positionForm').submit();
+  			});			
+		</script>
 		<?php include('ftr.php'); ?>
 	</div>
 </body>
